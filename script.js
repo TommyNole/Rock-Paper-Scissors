2e89fb357a9c074a4ea7dca9ea864019a10b83cb
TommyNole <tomcastle71@icloud.com>
2023-05-20 17:33:16 -0400

    Complete win logic and win/loss effects

--- script.js
+++ script.js
@@ -1,55 +1,73 @@
-
+const playerScore = document.querySelector('#player-score');
+const computerScore = document.querySelector('#computer-score');
+const statusText = document.querySelector('#status-text');
+const choiceBtns = document.querySelectorAll('.user-choices');
+const controlsPanel = document.querySelector('.controls');
+const endOfGamePanel = document.querySelector('.endofgame');
+const playAgainBtn = document.querySelector('#play-again');
+let playerChoice;
+let cpuChoice;
 let playerWins = 0;
 let computerWins = 0;
 
-// do {
-    const playerScore = document.querySelector('#player-score');
-    const computerScore = document.querySelector('#computer-score');
-    const statusText = document.querySelector('#status-text');
-    const choiceBtns = document.querySelectorAll('.user-choices');
-    let playerChoice;
-    let cpuChoice;
-    
-    choiceBtns.forEach(choiceBtn => choiceBtn.addEventListener('click', () => {
-        playerChoice = choiceBtn.id;
-        cpuChoice = computerChoice();
-        checkResult(playerChoice, cpuChoice);
-    }));
-            
-    function computerChoice() {
-        let choices = ['rock', 'paper', 'scissors'];
-        return choices[Math.floor(Math.random() * choices.length)];
+endOfGamePanel.style.display = 'none';
+
+choiceBtns.forEach(choiceBtn => choiceBtn.addEventListener('click', () => {
+    playerChoice = choiceBtn.id;
+    cpuChoice = computerChoice();
+    checkResult(playerChoice, cpuChoice);
+    checkForWin();
+}));
+        
+function computerChoice() {
+    let choices = ['rock', 'paper', 'scissors'];
+    return choices[Math.floor(Math.random() * choices.length)];
+}
+
+function checkResult(playerChoice, cpuChoice) { 
+    if (playerChoice === 'rock' && cpuChoice === 'scissors') {
+        statusText.textContent = 'You win! Rock beats scissors.';
+        playerScore.textContent++;
+        playerWins++;
+    } else if (playerChoice === 'scissors' && cpuChoice === 'paper') {
+        statusText.textContent = 'You win! Scissors beats paper.';
+        playerScore.textContent++;
+        playerWins++;
+    } else if (playerChoice === 'paper' && cpuChoice === 'rock') {
+        statusText.textContent = 'You win! Paper beats rock.';
+        playerScore.textContent++;
+        playerWins++;
+    } else if (playerChoice === cpuChoice) {
+        statusText.textContent = 'It\'s a tie!';
+    } else {
+        statusText.textContent = 'You lose! ' + cpuChoice + ' beats ' + playerChoice + '.';
+        computerScore.textContent++;
+        computerWins++;
     }
-    
-    function checkResult(playerChoice, cpuChoice) { 
-        if (playerChoice === 'rock' && cpuChoice === 'scissors') {
-            statusText.textContent = 'You win! Rock beats scissors.';
-            playerScore.textContent++;
-            playerWins++;
-        } else if (playerChoice === 'scissors' && cpuChoice === 'paper') {
-            statusText.textContent = 'You win! Scissors beats paper.';
-            playerScore.textContent++;
-            playerWins++;
-        } else if (playerChoice === 'paper' && cpuChoice === 'rock') {
-            statusText.textContent = 'You win! Paper beats rock.';
-            playerScore.textContent++;
-            playerWins++;
-        } else if (playerChoice === cpuChoice) {
-            statusText.textContent = 'It\'s a tie!';
-        } else {
-            statusText.textContent = 'You lose! ' + cpuChoice + ' beats ' + playerChoice + '.';
-            computerScore.textContent++;
-            computerWins++;
-        }
+}
+
+function checkForWin() {
+    if (playerWins === 5) {
+        statusText.textContent = 'You win the game!';
+        endOfGamePanel.style.display = 'block';
+        controlsPanel.style.display = 'none';
+    } else if (computerWins === 5) { 
+        statusText.textContent = 'You lose the game!';
+        endOfGamePanel.style.display = 'block';
+        controlsPanel.style.display = 'none';
     }
-// } while (playerWins < 5 && computerWins < 5);
-    
-// if (playerWins === 5) { 
-//     statusText.textContent = 'You win the game!';
-// } else {
-//     statusText.textContent = 'You lose the game!';
-// }
+        
+}
+
+playAgainBtn.addEventListener('click', () => { 
+    playerScore.textContent = 0;
+    computerScore.textContent = 0;
+    playerWins = 0;
+    computerWins = 0;
+    statusText.textContent = 'Make your choice!';
+    endOfGamePanel.style.display = 'none';
+    controlsPanel.style.display = 'flex';
+});
 
 
-            
-            
+    
